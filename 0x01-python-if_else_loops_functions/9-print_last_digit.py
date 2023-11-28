68afbf9f090ee3457ec60ee6a30726f94b474b9f
mohamed <mohamed@me.com>
2023-11-28 23:55:12 +0200

    9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    last = number
+    if last < 0:
+        last = last * -1
+        last = last % 10
+    else:
+        last = last % 10
+    print("{}".format(last), end="")
+    return last
