2335e0b927746fc17d9556f9ed3bb5d21b116666
mohamed <mohamed@me.com>
2023-12-02 17:53:45 +0200

    6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        pass
+    Row = len(matrix)
+    Column = len(matrix[i])
+
+    for row in range(Row):
+        for column in range(Column):
+            print(matrix[row][column], end=" ")
+        print()
