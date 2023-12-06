98c7e04cf70e5ffd787facebb646bac0de113a6f
mohamed <mohamed@me.com>
2023-12-07 00:28:10 +0200

    100

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    return [[i , replace][i is search] for i in my_list]
+    return [[i, replace][i is search] for i in my_list]
