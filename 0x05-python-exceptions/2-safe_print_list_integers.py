36dfd7fa26460e560e8fa4aac75056ee41444879
mohamed <mohamed@me.com>
2023-12-19 00:58:28 +0200

    2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except IndexError:
+            raise
+        except Exception:
+            pass
+        else:
+            c += 1
+    print()
+    return c
