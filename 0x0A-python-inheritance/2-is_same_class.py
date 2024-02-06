7bf62f8a71a518b92285b7701bed4fd0dfbcac3b
mohamed <mohamed@me.com>
2024-02-06 02:02:17 +0200

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""2-is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """define func
+
+    Args:
+        obj:object
+        a_class: first class
+
+    Returns:
+        type
+    """
+    return type(obj) == a_class
