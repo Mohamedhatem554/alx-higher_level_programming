8217f1ca354b27a5b664a2311fe333f1478d7391
mohamed <mohamed@me.com>
2024-02-06 03:30:27 +0200

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""11-square.py"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class square(Rectangle):
+    """class square
+
+    Args:
+        Rectangle (class): Rectangle class
+    """
+    def __init__(self, size):
+        """constructor
+
+        Args:
+            size (int): square size
+        """
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """function str
+
+        Returns:
+            Square size
+        """
+        return ("[Square] {:s}/{:s}".format(self.__size, self.__size,))
