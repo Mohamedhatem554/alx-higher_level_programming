e1592e8bc0f30a490f21d50ed86e24ca7c72683c
mohamed <mohamed@me.com>
2024-02-06 03:22:31 +0200

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""10-square.py"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class square(Rectangle):
+    """class square
+
+    Args:
+        Rectangle (class): Rectangle class
+    """
+    def __init__(self, size):
+        """constructor
+
+        Args:
+            size (int): the size of square
+        """
+        if self.integer_validator("size", size):
+            self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """function area
+
+        Returns:
+            the area
+        """
+        return super().area()
