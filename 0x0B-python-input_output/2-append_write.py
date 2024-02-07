5b2fd88b4977ffb95710d50bd0f2142bfdaf18ea
mohamed <mohamed@me.com>
2024-02-07 23:59:50 +0200

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,15 @@
 
 
 def append_write(filename="", text=""):
+    """append_write function
+
+    Args:
+        filename (str): write file name . Defaults to "".
+        text (str): write text. Defaults to "".
+
+    Returns:
+        number of characters
+    """
     with open(filename, "a", encoding="UTF-8") as f:
         num = f.write(text)
         return num
