ce61b42154c2372d983c2f7ca0cb129b88041760
mohamed <mohamed@me.com>
2024-02-07 23:32:36 +0200

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,10 @@
 
 
 def read_file(filename=""):
+    """ readfile function
+
+    Args:
+        filename (str): write a file name. Defaults to NULL.
+    """
     with open(filename, "r", encoding="UTF-8") as r:
         print(r.read(), end="")
