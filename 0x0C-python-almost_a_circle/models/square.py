69ffcacd6a8ab3f853db70010609ec6e5f498a5b
mohamed <mohamed@me.com>
2024-02-13 03:15:27 +0200

    10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,13 +11,13 @@ class Square(Rectangle):
 
     def __str__(self):
         """return the string"""
-        return "[Square] (<id>) <x>/<y> - <size>".\
+        return "[{}] ({}) {}/{} - {}".\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
         return self.width
-    
+
     @size.setter
     def size(self, v):
         self.width = v
